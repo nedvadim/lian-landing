bd0149760f1b6bafc96786f4538f19c45138a314
Vadim Nedelenko <nedvadim@gmail.com>
2020-01-22 15:20:24 +0200

    articles filter almost done

--- js/filterItemsInSectionTwo.js
+++ js/filterItemsInSectionTwo.js
@@ -1,8 +1,24 @@
 const btns = document.querySelectorAll(".partition");
-const articles = document.querySelectorAll(".grid-item");
 var grid = document.querySelector(".grid");
-var brandingArticles = document.getElementsByClassName(".brandings");
-console.log("brandingArticles " + brandingArticles[0]);
+const articles = document.querySelectorAll(".grid-item");
+
+const allArticles = document.querySelectorAll(".all");
+const brandingArticles = document.querySelectorAll(".branding");
+const logoArticles = document.querySelectorAll(".logo");
+const wPressArticles = document.querySelectorAll(".wPress");
+const webDesignArticles = document.querySelectorAll(".webDesign");
+const uiuxArticles = document.querySelectorAll(".uiux");
+
+const articlesList = [
+  { node: allArticles, name: "all" },
+  { node: brandingArticles, name: "branding" },
+  { node: logoArticles, name: "logo" },
+  { node: wPressArticles, name: "wPress" },
+  { node: webDesignArticles, name: "webDesign" },
+  { node: uiux, name: "uiux" }
+];
+
+// setting up masonry grid -------
 var msnry = new Masonry(grid, {
   itemSelector: ".grid-item",
   columnWidth: ".grid-sizer",
@@ -13,19 +29,31 @@ imagesLoaded(grid).on("progress", function() {
   msnry.layout();
 });
 
+// Attaching events to each button
 btns.forEach(btn => {
   btn.addEventListener("click", () => {
     articles.forEach(article => {
       article.remove();
-      console.log("removed: " + article);
       msnry.layout();
     });
     msnry.layout();
-    brandingArticles.forEach(art => {
-      console.log("appended: " + art);
-      grid.appendChild(art);
-      msnry.layout();
+
+    console.log("btn.id:  " + btn.id);
+
+    articlesList.forEach(article => {
+      if (article.name === btn.id) {
+        console.log(article);
+        article.node.forEach(node => {
+          grid.appendChild(node);
+          msnry.layout();
+        });
+      }
     });
+
+    // brandingArticles.forEach(art => {
+    //   grid.appendChild(art);
+    //   msnry.layout();
+    // });
 
     // appendBack needed articles and msnry.layout();
   });
